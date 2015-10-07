30b9502e4291c527b24d02d3c65a347d4ba770fa
ulucs <usengil@ku.edu.tr>
2015-10-07 20:48:25 +0300

    Functional details section

--- www/js/app.js
+++ www/js/app.js
@@ -24,7 +24,35 @@ angular.module('app', ['ionic', 'ionic.service.core'])
 
 // define services
 
-.factory('viewService', function($rootScope) {
+.factory('viewConfig', function() {
+	var viewConfig = {};
+
+	viewConfig.TRF = {
+		mainView: ["Plaka"],
+		mainViewSub: [],
+		detailedView: ["Sigortali","Plaka","PoliceNo","Marka","FfrkSir","Bitis"]
+	};
+
+	viewConfig.BES = {};
+	viewConfig.DSK = {};
+	viewConfig.HAY = {};
+	viewConfig.KKO = viewConfig.TRF;
+
+	viewConfig.KZA = {
+		mainView: ["PoliceNo"],
+		mainViewSub: [],
+		detailedView: ["Sigortali", "FfrkSir", "PoliceNo", "Bitis", "Brans"]
+	};
+
+	viewConfig.MHE = {};
+	viewConfig.NAK = {};
+	viewConfig.SAG = {};
+	viewConfig.YNG = {};
+
+	return viewConfig;
+})
+
+.factory('viewService', function($rootScope, viewConfig) {
 	var viewService = {};
 
 	viewService.currentView = {};
@@ -34,42 +62,52 @@ angular.module('app', ['ionic', 'ionic.service.core'])
 			case "BES":
 			viewService.currentView.title = "Bireysel Emeklilik";
 			viewService.currentView.policeler = $rootScope.userInfo.BES;
+			viewService.currentView.views = viewConfig.BES ;
 			break;
 			case "DSK":
 			viewService.currentView.title = "Dask";
 			viewService.currentView.policeler = $rootScope.userInfo.DSK;
+			viewService.currentView.views = viewConfig.DSK ;
 			break;
 			case "HAY":
 			viewService.currentView.title = "Hayat Sigortasi";
 			viewService.currentView.policeler = $rootScope.userInfo.HAY;
+			viewService.currentView.views = viewConfig.HAY ;
 			break;
 			case "KKO":
 			viewService.currentView.title = "Kasko";
 			viewService.currentView.policeler = $rootScope.userInfo.KKO;
+			viewService.currentView.views = viewConfig.KKO ;
 			break;
 			case "KZA":
 			viewService.currentView.title = "Kaza Sigortasi";
 			viewService.currentView.policeler = $rootScope.userInfo.KZA;
+			viewService.currentView.views = viewConfig.KZA ;
 			break;
 			case "MHE":
 			viewService.currentView.title = "Muhendislik Sigortasi";
 			viewService.currentView.policeler = $rootScope.userInfo.MHE;
+			viewService.currentView.views = viewConfig.MHE ;
 			break;
 			case "NAK":
-			viewService.currentView.title = "Nakliyet Sigortasi";
+			viewService.currentView.title = "Nakliyat Sigortasi";
 			viewService.currentView.policeler = $rootScope.userInfo.NAK;
+			viewService.currentView.views = viewConfig.NAK ;
 			break;
 			case "SAG":
 			viewService.currentView.title = "Saglik Sigortasi";
 			viewService.currentView.policeler = $rootScope.userInfo.SAG;
+			viewService.currentView.views = viewConfig.SAG ;
 			break;
 			case "TRF":
 			viewService.currentView.title = "Trafik Sigortasi";
 			viewService.currentView.policeler = $rootScope.userInfo.TRF;
+			viewService.currentView.views = viewConfig.TRF ;
 			break;
 			case "YNG":
 			viewService.currentView.title = "Yangin Sigortasi";
 			viewService.currentView.policeler = $rootScope.userInfo.YNG;
+			viewService.currentView.views = viewConfig.YNG ;
 			break;
 		}
 	};
@@ -192,10 +230,15 @@ angular.module('app', ['ionic', 'ionic.service.core'])
 	};
 })
 
-.controller('policeController', function($scope,$ionicModal,viewService) {
+.controller('policeController', function($scope,$ionicModal,$state,viewService) {
 
 	$scope.policeler = viewService.currentView.policeler;
 	$scope.title =  viewService.currentView.title;
+	$scope.views = viewService.currentView.views;
+
+	$scope.viewDetails = function(police) {
+		$state.go('policeDetaylari', {viewData: police, viewConf: $scope.views.detailedView});
+	}
 
 	$ionicModal.fromTemplateUrl('kasko-modal.html', {
 			scope: $scope,
@@ -226,6 +269,13 @@ angular.module('app', ['ionic', 'ionic.service.core'])
 	});
 })
 
+.controller('pdController', function($scope,$stateParams) {
+
+	$scope.police = $stateParams.viewData;
+	$scope.conf = $stateParams.viewConf;
+
+})
+
 .config(function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider
@@ -251,6 +301,16 @@ angular.module('app', ['ionic', 'ionic.service.core'])
 			url: '/policeler',
 			templateUrl: 'genericPolice.html',
 			controller: 'policeController'
+		})
+
+		.state('policeDetaylari', {
+			url: '/detay',
+			templateUrl: 'policeDetay.html',
+			controller: 'pdController',
+			params: { 
+				viewData: null,
+				viewConf: null
+			}
 		});
 	
 	$urlRouterProvider.otherwise('');
